be9d25ed8a77950a7d98c21a6e9d8bfb2f9dc914
itsYssp <infotect.markharesco@gmail.com>
2025-10-20 22:57:33 +0800

    commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
+
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -7,3 +11,28 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'app/about.html'
+
+class BlogListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'app/blog_list.html'
+
+class BlogDetailView(DetailView):
+    model =  Post
+    context_object_name = 'post'
+    template_name = 'app/blog_details_view.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    fields = ['title', 'author', 'body']
+    template_name = 'app/blog_create.html'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'author', 'body']
+    template_name = 'app/blog_update.html'
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'app/blog_delete.html'
+    success_url = reverse_lazy('home')
